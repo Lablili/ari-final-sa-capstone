149ffe9e0785b1214517feec3d11ebf34e98782e
Lablili <lovelyreyes460@gmail.com>
2026-07-18 12:00:00 +0800

    style: remove blue accent from dashboard, shrink cards, hide archive panel by default

--- wwwroot/js/fisherfolk-info.js
+++ wwwroot/js/fisherfolk-info.js
@@ -474,6 +474,21 @@
         registerFormModal.classList.remove("hidden");
     });
 
+    var btnToggleArchive = document.getElementById("btnToggleArchive");
+    var archivePanelSection = document.getElementById("archivePanelSection");
+    if (btnToggleArchive && archivePanelSection) {
+        btnToggleArchive.addEventListener("click", function () {
+            if (archivePanelSection.style.display === "none") {
+                archivePanelSection.style.display = "block";
+                btnToggleArchive.innerHTML = '<svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path stroke-linecap="round" stroke-linejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path></svg> Hide Archive';
+            } else {
+                archivePanelSection.style.display = "none";
+                btnToggleArchive.innerHTML = '<svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path stroke-linecap="round" stroke-linejoin="round" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg> Show Archive';
+            }
+        });
+    }
+
+
     function closeRegisterForm() {
         registerFormModal.classList.add("hidden");
     }
